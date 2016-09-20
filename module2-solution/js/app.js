1c810b17ad867f2339cb1f8790329c35b5b8e52b
Will Sterling <will.sterling@wowza.com>
2016-09-20 00:09:30 -0600

    Initial commit of module 2 solution.

---
+++ module2-solution/js/app.js
@@ -0,0 +1,61 @@
+(function () {
+'use strict';
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyListController', ToBuyListController)
+.controller('BoughtListController', BoughtListController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyListController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyListController(ShoppingListCheckOffService) {
+  var toBuyList=this;
+
+  toBuyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+  toBuyList.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  };
+
+}
+
+BoughtListController.$inject = ['ShoppingListCheckOffService'];
+function BoughtListController(ShoppingListCheckOffService) {
+  var boughtList=this;
+
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [];
+  var boughtItems = [];
+
+  service.addToBuyItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity,
+    };
+    toBuyItems.push(item);
+  };
+
+  service.addToBuyItem("Lettuce", 1)
+  service.addToBuyItem("Cheese", 1)
+
+
+  service.buyItem = function (itemIndex) {
+    var item=toBuyItems[itemIndex];
+    boughtItems.push(item);
+    toBuyItems.splice(itemIndex, 1);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
